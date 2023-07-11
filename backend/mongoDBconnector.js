0065a3e56b3b293a13d71b4a783575bf05addb48
Solomoni Railoa <90390564+solomonirailoa@users.noreply.github.com>
2023-07-12 07:50:13 +1200

    Update mongoDBconnector.js

--- backend/mongoDBconnector.js
+++ backend/mongoDBconnector.js
@@ -19,12 +19,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+	const database = client.db("sample_mflix");
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await database.command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	
+    const movies = database.collection("movies");
+    // Query for a movie that has the title 'The Room'
+    const query = { title: "The Room" };
+    const options = {
+      // sort matched documents in descending order by rating
+      sort: { "imdb.rating": -1 },
+      // Include only the `title` and `imdb` fields in the returned document
+      projection: { _id: 0, title: 1, imdb: 1 },
+    };
+    const movie = await movies.findOne(query, options);
+    // since this method returns the matched document, not a cursor, print it directly
+    console.log(movie);
+	
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
   }
 }
 run().catch(console.dir);
+
