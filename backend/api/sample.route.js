c8e46d099da43db9647e562d84cbca8bdd6c3916
Solomoni Railoa <90390564+solomonirailoa@users.noreply.github.com>
2023-07-12 08:36:16 +1200

    Update sample.route.js

--- backend/api/sample.route.js
+++ backend/api/sample.route.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-router.route("/").get((req,res)=> res.status(202).json({group:"webler",allies:"sia",enemies:"sololearn"});
+router.route("/").get((req,res)=> res.status(202).json({group:"webler",allies:"sia",enemies:"sololearn"}));
 
 module.exports = router;
