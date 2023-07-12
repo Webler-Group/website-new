83b9254e576b373c996c29cb0e647bf0d4de8f31
DonDejvo <dada542cz@gmail.com>
2023-07-12 02:27:03 +0200

    Origins Updated

--- backend/dist/config/allowedOrigins.js
+++ backend/dist/config/allowedOrigins.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const allowedOrigins = [
     "http://localhost:5500",
-    "https://api-webler.onrender.com/",
+    "https://api-webler.onrender.com",
+    "https://webler.onrender.com",
     ""
 ];
 exports.default = allowedOrigins;
--- backend/src/config/allowedOrigins.ts
+++ backend/src/config/allowedOrigins.ts
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "http://localhost:5500",
-    "https://api-webler.onrender.com/",
+    "https://api-webler.onrender.com",
+    "https://webler.onrender.com",
     ""
 ];
 
