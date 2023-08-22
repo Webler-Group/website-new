38027eb2b31f21f012163df818520979ea23a6da
DonDejvo <dada542cz@gmail.com>
2023-08-22 15:14:11 +0200

    cors allow chillpillcoding

--- backend/src/config/allowedOrigins.ts
+++ backend/src/config/allowedOrigins.ts
@@ -1,18 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const allowedOrigins = 
-process.env.NODE_ENV === "production" ?
-[
-    "https://api-webler.onrender.com",
-    "https://webler.onrender.com"
-] :
-[
-    "http://localhost:5500",
-    "http://localhost:5173",
-    "https://api-webler.onrender.com",
-    "https://webler.onrender.com",
-    ""
-];
+const allowedOrigins =
+    process.env.NODE_ENV === "production" ?
+        [
+            "http://142.11.239.109",
+            "http://chillpillcoding.com",
+        ] :
+        [
+            "http://localhost:5500",
+            "http://localhost:5173",
+            ""
+        ];
 
 export default allowedOrigins;
--- backend/src/middleware/logger.ts
+++ backend/src/middleware/logger.ts
@@ -3,18 +3,24 @@ import { v4 as uuid } from "uuid";
 import fs from "fs";
 import path from "path";
 import { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const logEvents = async (message: string, logFileName: string) => {
+    if (process.env.NODE_ENV !== "development") {
+        return;
+    }
     const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss');
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
     try {
-        if(!fs.existsSync(path.join(__dirname, "..", "..", "logs"))) {
+        if (!fs.existsSync(path.join(__dirname, "..", "..", "logs"))) {
             await fs.promises.mkdir(path.join(__dirname, "..", "..", "logs"));
         }
         await fs.promises.appendFile(path.join(__dirname, "..", "..", "logs", logFileName), logItem);
     }
-    catch(err) {
+    catch (err) {
         console.log(err);
     }
 }
