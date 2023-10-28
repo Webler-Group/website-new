b7fc64ad8e2842cf9391ced64fdf6a4321f4288a
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2023-10-27 21:47:51 -0500

    Update TermsOfUse.tsx

--- frontend/src/pages/TermsOfUse.tsx
+++ frontend/src/pages/TermsOfUse.tsx
@@ -23,7 +23,10 @@ const TermsOfUse = () => {
                         <p>Any children below age 16 who register on this website, will be banned and account terminated.</p>
                         <p>All users can be banned at any time and for any reason.</p>
                         <p>This website is not a code repo. Your data may be lost someday. You probably should back up the important stuff else where</p>
-                        
+                        <p>Users should not try to promote a youtube channel. That may expedite a ban for the user who does this.</p>
+                        <p>Users should not publish anything that violates copyright. We take intellectual property very seriously.</p>
+                        <p>Users should be respectful towards one another. A failure to do so, may expedite a ban for the user who is disrespectful.</p>
+                        <p>Users should keep all content matters coding related: no politics please!.</p>
                     </div>
                 </div>
             </div>
