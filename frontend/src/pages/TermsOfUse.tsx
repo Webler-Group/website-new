0af4ceeff16a9746f1073c774c9bfae1d3016d42
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2023-10-27 22:23:01 -0500

    Update TermsOfUse.tsx

--- frontend/src/pages/TermsOfUse.tsx
+++ frontend/src/pages/TermsOfUse.tsx
@@ -20,7 +20,7 @@ const TermsOfUse = () => {
                         <br />
                         <p>Webler or chillpillcoding does not give any warantee or garantee as to the accuracy of its content.</p>
                         <p>Webler or chillpillcoding is not liable, not responsible for any damages or loss you may have incurred while using our website.</p>
-                        <p>Any children below age 16 who register on this website, will be banned and account terminated.</p>
+                        <p>Any children below age 18 who register on this website, will be banned and account terminated.</p>
                         <p>All users can be banned at any time and for any reason.</p>
                         <p>This website is not a code repo. Your data may be lost someday. You probably should back up the important stuff else where</p>
                         <p>Users should not try to promote a youtube channel. That may expedite a ban for the user who does this.</p>
