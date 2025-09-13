3b58efa2c18ae1a91a8c84348cd28f549f0262c3
DonDejvo <dada542cz@gmail.com>
2025-09-13 18:30:09 +0200

    update

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,8 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 import { logEvents } from "./logger";
 import { CastError } from "mongoose";
+import { ZodError } from "zod";
 
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof ZodError) {
+    res.status(400).json({
+      success: false,
+      message: err.issues,
+    });
+    return
+  }
+
     logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log');
     console.log(err.stack);
 
