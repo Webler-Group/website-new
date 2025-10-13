a39f6ccd9009ef1e6206d978f9c1366ae50e74a6
PhyG0 <zebra9989zebra@gmail.com>
2025-10-13 10:43:42 +0530

    Tailwind setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
-    allowedHosts: [
-      "weblercodes.com"
-    ],
+    host: true, 
     proxy: {
       "/api": {
         target: "http://localhost:5500",
