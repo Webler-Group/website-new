38fff1233edfcb0a721c561d3361e1161e4c6967
Methylol <88193952+Methylol@users.noreply.github.com>
2026-04-24 17:28:55 +0330

    Update ChannelsList2.tsx

--- frontend/src/features/channels/components/ChannelsList2.tsx
+++ frontend/src/features/channels/components/ChannelsList2.tsx
@@ -167,9 +167,8 @@ const ChannelsList2 = ({ onChannelSelect, currentChannelId, onExit }: ChannelsLi
                         <Nav.Item className="position-relative">
                             <Nav.Link size="sm" className="border border-primary" eventKey="channels">Channels</Nav.Link>
                         </Nav.Item>
-                        <Nav.Item className="position-relative">
-                            <Nav.Link size="sm" className="border border-primary" eventKey="invites">Invites</Nav.Link>
-
+                        <Nav.Item className="position-relative small">
+                            <Nav.Link className="border border-primary py-1 px-2" eventKey="channels">Channels</Nav.Link>
                             {invites.totalCount > 0 && (
                                 <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                                     {invites.totalCount > 99 ? "99+" : invites.totalCount}
